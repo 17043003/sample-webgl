019d7e2e87fbade1ed85054120445aa58ef4888d
17043003 <ishizuka533725@gmail.com>
2022-02-01 05:41:12 +0900

    add: calculate two vector from vertices.

--- next-gl/utils/calculate.ts
+++ next-gl/utils/calculate.ts
@@ -1,5 +1,14 @@
 const calculateNormal = (vertices: number[], indices: number[]) => {
-    
+    const normals: number[] = []
+    for(let i = 0; i < indices.length; i += 3){        
+        const v1 = [0, 1, 2].map((it) => {
+            return vertices[3 * indices[i + 2] + it] - vertices[3 * indices[i + 1] + it];
+        });
+
+        const v2 = [0, 1, 2].map((it) => {
+            return vertices[3 * indices[i] + it] - vertices[3 * indices[i + 1] + it];
+        });
+    }
 }
 
 export default calculateNormal
